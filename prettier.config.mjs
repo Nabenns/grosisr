84d72fa27903fddeaddb6140109a3fa286c8b0a3
Nabenns <nabhanggt@gmail.com>
2026-05-28 17:45:12 +0700

    chore: add eslint flat config + prettier config + env keys (task 1 polish)

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: false,
+  trailingComma: "none",
+  printWidth: 120,
+  arrowParens: "always"
+}
+
+export default config
